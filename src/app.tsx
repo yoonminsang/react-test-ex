f825514199d9f33e84b853a43194ecb7ca00319d
yoonminsang <apdlyooapdl@naver.com>
2021-12-11 02:14:22 +0900

    feat: app에 하위 컴포넌트 적용

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+import CountEx from './components/count-ex';
+import FormEx from './components/form-ex';
+
+const Wrapper = styled.div`
+  margin: 10px;
+`;
+
+const Margin = styled.div`
+  height: 50px;
+`;
 
 const App: React.FC = () => {
-  return <div>앱</div>;
+  const onSubmit = (email: string, password: string) => {
+    console.log(email, password);
+  };
+
+  return (
+    <Wrapper>
+      앱
+      <CountEx />
+      <Margin />
+      <FormEx onSubmit={onSubmit} />
+    </Wrapper>
+  );
 };
 
 export default App;
