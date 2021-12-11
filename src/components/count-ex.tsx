fc80c4f68424860177c3e723782c5e8083ea446a
yoonminsang <apdlyooapdl@naver.com>
2021-12-11 14:34:33 +0900

    wip: count 컴포넌트 usecallback 적용

--- src/components/count-ex.tsx
+++ src/components/count-ex.tsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div``;
 
 const CountEx: React.FC = () => {
   const [count, setCount] = useState(0);
-  const onIncrease = () => {
-    setCount(count + 1);
-  };
-  const onDecrease = () => {
-    setCount(count - 1);
-  };
+  const onIncrease = useCallback(() => {
+    setCount((count) => count + 1);
+  }, []);
+  const onDecrease = useCallback(() => {
+    setCount((count) => count - 1);
+  }, []);
   return (
     <Wrapper>
       <button type="button" onClick={onIncrease}>
